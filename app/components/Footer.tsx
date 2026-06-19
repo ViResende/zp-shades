951583e638d671f30b7122787737782d9e962fbf
Vitoria Resende <[vitoriacarolineresende1@gmail.com](mailto:vitoriacarolineresende1@gmail.com)>
2026-06-18 21:29:04 -0700

    removed about of foot

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -15,7 +15,6 @@ export default function Footer() {
               <li><Link href="/" className="hover:text-white transition-colors">Home</Link></li>
               <li><Link href="/services" className="hover:text-white transition-colors">Services</Link></li>
               <li><Link href="/gallery" className="hover:text-white transition-colors">Gallery</Link></li>
-              <li><Link href="/about" className="hover:text-white transition-colors">About</Link></li>
               <li><Link href="/contact" className="hover:text-white transition-colors">Contact</Link></li>
               <li><Link href="/book" className="hover:text-white transition-colors">Book a Service</Link></li>
             </ul>
